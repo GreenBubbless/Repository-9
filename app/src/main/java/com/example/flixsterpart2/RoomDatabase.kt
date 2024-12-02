900e4550b56027b9172f22235e9809207d295e6b
Nikolas Buckle <nikolasmbuckle@gmail.com>
2024-12-02 16:02:52 -0500

    first commit

--- app/src/main/java/com/example/flixsterpart2/RoomDatabase.kt
+++
@@ -1,8 +0,0 @@
-import androidx.room.Database
-import androidx.room.RoomDatabase
-import com.example.flixsterpart2.CommentEntity
-
-@Database(entities = [CommentEntity::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun commentDao(): CommentDao
-}
